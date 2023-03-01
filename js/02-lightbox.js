69fa4f4053fcaed65d2c4a2ea0152cc8827b6eef
Serhii-Buha <serhii.buga@gmail.com>
2023-03-01 14:12:09 +0200

    Assignment 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const imageEl = [];
+
+galleryItems.forEach((galleryItem) => {
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = galleryItem.original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = galleryItem.preview;
+  image.alt = galleryItem.description;
+
+  link.appendChild(image);
+
+  imageEl.push(link);
+});
+
+galleryEl.append(...imageEl);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
