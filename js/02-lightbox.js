f655b0a3b81f10ec0f360e8280519311d99561da
Serhii-Buha <serhii.buga@gmail.com>
2023-03-03 13:21:04 +0200

    v2-hw-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -28,3 +28,29 @@ const lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
   captionsData: "alt",
 });
+
+//v2
+
+// const galleryEl = document.querySelector(".gallery");
+
+// const imageEl = galleryItems.map((galleryItem) => {
+//   const link = document.createElement("a");
+//   link.classList.add("gallery__link");
+//   link.href = galleryItem.original;
+
+//   const image = document.createElement("img");
+//   image.classList.add("gallery__image");
+//   image.src = galleryItem.preview;
+//   image.alt = galleryItem.description;
+
+//   link.appendChild(image);
+
+//   return link;
+// });
+
+// galleryEl.append(...imageEl);
+
+// const lightbox = new SimpleLightbox(".gallery a", {
+//   captionDelay: 250,
+//   captionsData: "alt",
+// });
